e368d278d2240b18c3eb6d0f54c9580cfabd2196
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-16 17:38:20 +0100

    esercizio

---
+++ classes/Antipulci.php
@@ -0,0 +1,20 @@
+<?php
+
+//includo la classe prodotto nella classe Antipulce
+require_once __DIR__ . '/Prodotto.php';
+
+class Antipulci extends Prodotto{
+    protected $disponibilità;
+
+    public function __construct($nome, $sconto = null, $pezzi = null, $codiceProdotto = null,$disponibilità){
+        //valori ripresi dalla classe padre
+        parent::__construct($nome, $sconto, $pezzi , $codiceProdotto );
+
+        //valori nuovi
+        if(date("m") >= 05 && date("m") =< 08){
+            $this->$disponibilità = "disponibilie";
+        }else{
+            $this->$disponibilità = "Non disponibilie";
+        }
+    }
+}
--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 require_once __DIR__ . '/classes/CartaDiCredito.php';
 require_once __DIR__ . '/classes/Cibo.php';
 require_once __DIR__ . '/classes/Cucce.php';
+require_once __DIR__ . '/classes/Antipulci.php';
 require_once __DIR__ . '/classes/Giochi.php';
+require_once __DIR__ . '/classes/User.php';
+require_once __DIR__ . '/classes/LoggedUser.php';
 
 $carta1 = new CartaDiCredito("Pierluigi", "La Rocca", 8000800080008000,04,24,666);
 var_dump($carta1);
@@ -16,3 +19,9 @@ var_dump($Cuccia1);
 
 $Gioco1 = new Giochi("Trixie",15,75,007,'plastica');
 var_dump($Gioco1);
+
+$UserOspite1 = new User("Luigi","Rossi","luigirossi@email.it", "Via corso 2",3405745600);
+var_dump($UserOspite1);
+
+$UserLogged1 = new LoggedUser("Luca","Scovotto","lucascovotto@email.it","Via corso 3",3405745601,10);
+var_dump($UserLogged1);
