97fc3e2c6629a4e7c60666829bc52a1028b5e58b
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-16 17:05:37 +0100

    Creazione delle istanze per le classi create

--- classes/CartaDiCredito.php
+++ classes/CartaDiCredito.php
@@ -14,21 +14,21 @@ class CartaDiCredito{
         if (is_string($nome) && strlen($nome) > 3){
             $this->nome = $nome;
         }else{
-            echo 'Error';
+            echo 'Error nome non valido';
         }
 
         //contollo del cognome
         if (is_string($cognome) && strlen($cognome) > 3){
             $this->cognome = $cognome;
         }else{
-            echo 'Error';
+            echo 'Error cognome non valido';
         }
 
         //controllo del numero della carta
         if (is_numeric($numeroCarta) && $numeroCarta > 1000000000000000){
             $this->numeroCarta = $numeroCarta;
         }else{
-            echo 'Error';
+            echo 'Error numero di carta non valido';
         }
 
         //controllo del mese di scadenza
@@ -36,14 +36,14 @@ class CartaDiCredito{
         if(is_numeric($meseScadenza) && $meseScadenza >= date("m") + 1 ){
             $this->meseScadenza = $meseScadenza;
         }else{
-            echo 'Error';
+            echo 'Error mese non valido o carta scaduta';
         }
 
         //controllo dell' anno di scadenza
         if(is_numeric($annoScadenza) && $annoScadenza >= date("y") ){
             $this->annoScadenza = $annoScadenza;
         }else{
-            echo 'Error';
+            echo 'Error anno non valido o carta scaduta';
         }
 
         //controllo del cvc
@@ -51,7 +51,7 @@ class CartaDiCredito{
         if(is_numeric($cvc) && $cvc >= 001 ){
             $this->cvc = $cvc;
         }else{
-            echo 'Error';
+            echo 'Error cvc non valido ';
         }
         
     }
--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+require_once __DIR__ . '/classes/CartaDiCredito.php';
+require_once __DIR__ . '/classes/Cibo.php';
+require_once __DIR__ . '/classes/Cucce.php';
+require_once __DIR__ . '/classes/Giochi.php';
+
+$carta1 = new CartaDiCredito("Pierluigi", "La Rocca", 8000800080008000,04,24,666);
+var_dump($carta1);
+
+$ScatoloCibo1 = new Cibo("Simba", '',25,001,250,"Carne di Manzo");
+var_dump($ScatoloCibo1);
+
+$Cuccia1 = new Cucce("Croci Villa", 5, 10, 005, 'legno', 255,300);
+var_dump($Cuccia1);
+
+$Gioco1 = new Giochi("Trixie",15,75,007,'plastica');
+var_dump($Gioco1);
