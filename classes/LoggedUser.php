d4028bbc1211b96cc6b0a7d71e81e26546ea0758
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-16 17:23:54 +0100

    Creata classe logged user

---
+++ classes/LoggedUser.php
@@ -0,0 +1,17 @@
+<?php
+
+//includo la classe user nella logged user
+require_once __DIR__ . '/User.php';
+
+class LoggedUser extends User{
+    protected $sconto = 20;
+
+    public function __construct($nome,$cognome,$email,$Indirizzo,$numeroTelefono,$sconto){
+        //valori ripresi dalla classe padre
+        parent::__construct($nome,$cognome,$email,$Indirizzo,$numeroTelefono );
+
+        //valori nuovi
+        $this->sconto = $sconto;
+        
+    }
+}
